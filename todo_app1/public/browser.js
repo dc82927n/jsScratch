ec9c9f4474faf0ef93eccffb7e755fa036f66710
David <dc82927n@gmail.com>
2019-12-01 15:29:54 -0500

    almost finish to the app

--- todo_app1/public/browser.js
+++ todo_app1/public/browser.js
@@ -1,16 +1,39 @@
 
 
 
-	document.addEventListener("click", function(e) {
-		if (e.target.classList.contains("edit-me")) {
-         let userInput = prompt("enter you new field of text")
-         axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")} ).then(function() {
-            }).catch(function() {
-            console.log("please try again later") 
-            })
-    
-		}
-    })
+document.addEventListener("click", function (e) {
+ 
+    // add the delete me update. 
+    if (e.target.classList.contains("delete-me")) { 
+
+      if (confirm(" do you want to delete? ")) {
+      axios.post('/delete-item', {id: e.target.getAttribute("data-id") }).then(function () {
+      e.target.parentElement.parentElement.remove()
+      
+        }).catch(function () {
+        console.log("please try again later")
+        })
+      
+      }
+    }
+
+
+    // creating a user input and updating it. 
+    if (e.target.classList.contains("edit-me")) {
+      let userInput = prompt("enter you new field of text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+  
+    if (userInput) {
+      axios.post('/update-item', { text: userInput, id: e.target.getAttribute("data-id") }).then(function () {
+        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+      }).catch(function () {
+        console.log("please try again later")
+      })
+    }
+
+  }
+})
+
+
 
 
 
--- todo_app1/server.js
+++ todo_app1/server.js
@@ -15,8 +15,6 @@ mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: t
 })
 
 
-
-
 app.use(express.json())   // now we want it to be asyncronix req
 app.use(express.urlencoded({ extended: false })) //this is telling the express that uses the req.body object to the server
 
@@ -49,8 +47,8 @@ app.get('/', function (req, res) {
                 <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
 	            <span class="item-text">${item.text}</span>
 	            <div>
-	              <button data-id="${item._id}" btn btn-secondary btn-sm mr-1">Edit</button>
-	              <button class="delete-me btn btn-danger btn-sm">Delete</button>
+	              <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+	              <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
 	            </div>
 	          </li>
             `
@@ -68,20 +66,31 @@ app.get('/', function (req, res) {
 
 })
 
-    app.post('/create-item', function (req, res) {
-        db.collection('item').insertOne({text: req.body.item }, function () {
-            res.redirect("/")
-        })
+app.post('/create-item', function (req, res) {
+    db.collection('item').insertOne({text: req.body.item }, function () {
+        res.redirect("/")
     })
+})
 
-    /* this going to allow the user to update the edit, grab the data from the database and update the data.  */
-    app.post('/update-item', function(req, res){
-        db.collection('item').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set:{text: req.body.text}},function(){
-            res.send("sucess")
+/* this going to allow the user to update the edit, grab the data from the database and update the data.  */
+app.post('/update-item', function(req, res){
+    db.collection('item').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set:{text: req.body.text}},function(){
+        res.send("sucess")
 
-        })
     })
+})
 
+
+
+app.post('/delete-item', function(req, res){
+  db.collection('item').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, {$set:{text: req.body.text}}, function(){
+    res.send("sucess")
+  })
+})
+
+
+
+  
 
 
 
